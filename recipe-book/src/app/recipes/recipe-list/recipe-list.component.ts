32f977ce10e2f7ff428a979f84594901450bde51
James Sinnott <JamesRichardSinnott@gmail.com>
2024-02-21 16:34:16 +0000

    [ADD] HTML for displaying recipes

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class RecipeListComponent {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://glutenfreecuppatea.co.uk/wp-content/uploads/2022/01/gluten-free-smoky-paprika-chicken-recipe-featured-500x500.jpg")
+  ];
 
 }
