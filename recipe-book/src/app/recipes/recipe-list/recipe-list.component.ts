bb95422bd39a1cb5ef4459b41a6a6015ca4594ee
James Sinnott <JamesRichardSinnott@gmail.com>
2024-02-22 16:27:12 +0000

    [ADD] Using ngFor to output list of recipes

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent {
 
   recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://glutenfreecuppatea.co.uk/wp-content/uploads/2022/01/gluten-free-smoky-paprika-chicken-recipe-featured-500x500.jpg"),
     new Recipe("A Test Recipe", "This is simply a test", "https://glutenfreecuppatea.co.uk/wp-content/uploads/2022/01/gluten-free-smoky-paprika-chicken-recipe-featured-500x500.jpg")
   ];
 
