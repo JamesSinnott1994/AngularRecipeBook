94bcea77a0531a4ebdf276f8ce9441aba7baaab6
James Sinnott <JamesRichardSinnott@gmail.com>
2024-04-21 11:11:44 +0100

    [ADD] Using Services for Pushing Data from A to B

--- recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -18,6 +18,13 @@ export class ShoppingListComponent {
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
+
+    this.slService.ingredientsChanged
+      .subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients;
+        }
+      );
   }
 
 }
--- recipe-book/src/app/shopping-list/shopping-list.service.ts
+++ recipe-book/src/app/shopping-list/shopping-list.service.ts
@@ -1,7 +1,10 @@
+import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
     
+    ingredientsChanged = new EventEmitter<Ingredient[]>(); // Type it passes on is an array of ingredients
+
     private ingredients = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
@@ -12,7 +15,12 @@ export class ShoppingListService {
     }
 
     addIngredient(ingredient: Ingredient) {
+        // Adding doesn't work in the browser as we are returning a copy above
+        // We need to inform our component of the change
         this.ingredients.push(ingredient);
+
+        // Now we always have the right ingredients and inform other components about this in the event emitter here
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 
 }
